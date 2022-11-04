8d2d990c5f7c8b894dc935888af0a28f03d223bf
Cristian Muresan <muresan.cristian2000@gmail.com>
2022-11-04 23:31:16 +0200

    Add Tutorial Changes

---
+++ Models/Book.cs
@@ -0,0 +1,15 @@
+﻿namespace Muresan_Cristian_Lab2.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public decimal Price { get; set; }
+
+        public ICollection<Order> Orders { get; set; }
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿namespace Muresan_Cristian_Lab2.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public ICollection<Order> Orders { get; set; }
+    }
+}
